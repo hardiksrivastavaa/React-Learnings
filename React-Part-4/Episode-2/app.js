258c21773980a2cfdc26d6ee1bb76e530a3f3194
hardiksrivastavaa <hardikfgp@gmail.com>
2025-05-29 09:21:34 +0530

    Episode-2

---
+++ React-Part-4/Episode-2/app.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("p", { id: "para1" }, "hi I am Para 1"),
+        React.createElement("p", { id: "para2" }, "hi I am Para 2"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("p", { id: "para1" }, "hi I am Para 3"),
+        React.createElement("p", { id: "para2" }, "hi I am Para 4"),
+    ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
